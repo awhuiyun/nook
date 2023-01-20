4b91dfd97626951c44f94ba1275912f0332dc082
awhuiyun <117333975+awhuiyun@users.noreply.github.com>
2023-01-20 16:25:04 +0800

    Added NavBar (no links)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
-  return <div></div>;
+  return (
+    <div className="flex flex-row h-[80px] content-center backdrop-blur-sm sticky top-0">
+      <p className="text-zinc-700 mx-14 my-5 text-3xl self-center ">Nook</p>
+      <div className="border rounded border-zinc-700 w-72 py-2 m-5 self-center">
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          className="text-zinc-300 px-2"
+        />
+        <input
+          className="focus:outline-none placeholder:text-zinc-300 self-center"
+          type="text"
+          placeholder="Search"
+        />
+      </div>
+      <div className="flex flex-row grow justify-end">
+        <p className="text-zinc-500py-2 px-4 m-5 rounded self-center">
+          Explore
+        </p>
+        <p className="bg-zinc-700 text-[white] py-2 px-4 ml-5 mr-14 my-5 rounded self-center">
+          Publish
+        </p>
+      </div>
+    </div>
+  );
 }
