b9882fa80dcd42d18e569853befc39b1081386ea
awhuiyun <117333975+awhuiyun@users.noreply.github.com>
2023-01-21 18:01:14 +0800

    Added GET request to fetch notion data in BookContents component

--- src/components/BookContents.jsx
+++ src/components/BookContents.jsx
@@ -1,20 +1,42 @@
-import React from "react";
-import dummyBookContent from "../datasets/dummyBookContent";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import HeadingOne from "./HeadingOne";
 import HeadingTwo from "./HeadingTwo";
 import HeadingThree from "./HeadingThree";
 import Paragraph from "./Paragraph";
+import bookDetailsArray from "../datasets/bookDetailsArray";
 
 export default function BookContents({ bookKey }) {
-  const bookContent = dummyBookContent.filter((item) => {
-    return item.bookKey === bookKey;
-  })[0].bookContent;
+  // Book details
+  const bookDetails = bookDetailsArray.filter((item) => {
+    return item.key === bookKey;
+  })[0];
 
-  // console.log(bookContent);
+  // Fetch dummy notion content
+  const [bookData, setBookData] = useState([]);
+
+  // GET request function to backend
+  function getData() {
+    axios
+      .get("http://localhost:5000/notion-data", {
+        params: {
+          // id: bookDetails.notionPageId,
+          id: "c47e8eba8c5e4e18ae760376a7a4a67e",
+        },
+      })
+      .then((res) => setBookData(res.data));
+  }
+
+  // Get data on mount
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(bookData);
 
   return (
     <div className="text-zinc-700">
-      {bookContent.map((item, index) => {
+      {bookData.map((item, index) => {
         if (item.type === "heading_1") {
           return <HeadingOne key={index} content={item.content} />;
         } else if (item.type === "heading_2") {
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,8 @@ export default function Footer({ bookKey }) {
   return (
     <div className="h-[80px] bg-zinc-700 sticky bottom-0 flex flex-row justify-center mt-16">
       <p className="text-white text-2xl pt-[24px]">
-        Support the author for ${bookDetails.bookPrice} &#128073;
+        Unlock the book and support the author for ${bookDetails.bookPrice}{" "}
+        &#128073;
       </p>
       <button className="bg-red-400 text-white py-2 px-4 mx-5 my-5 rounded self-center">
         Checkout
