55a395886898ca41c1bfc6e2b74c12b4fbf7bae2
awhuiyun <117333975+awhuiyun@users.noreply.github.com>
2023-01-22 15:23:14 +0800

    Added obfuscation logic

--- src/components/BookContents.jsx
+++ src/components/BookContents.jsx
@@ -11,42 +11,100 @@ export default function BookContents({ bookKey }) {
   const bookDetails = bookDetailsArray.filter((item) => {
     return item.key === bookKey;
   })[0];
+  const percentToObfuscate = parseFloat(bookDetails.percentObfuscated) / 100;
 
-  // Fetch dummy notion content
+  // Fetch notion content
   const [bookData, setBookData] = useState([]);
+  const [obfuscatedBookData, setObfuscatedBookData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  // GET request function to backend
+  // Function to obfuscate data
+  function obfuscateData(data) {
+    const characters = "abcdefghijklmnopqrstuvwxyz";
+
+    if (data.length > 0) {
+      for (let i = 0; i <= data.length - 1; i++) {
+        const percentOfBook = i / data.length;
+        if (
+          data[i].type !== "heading_1" &&
+          percentOfBook >= 1 - percentToObfuscate
+        ) {
+          let editedContent = "";
+          for (let j = 0; j <= data[i].content.length - 1; j++) {
+            if (data[i].content[j].match(/[a-z]/i)) {
+              if (data[i].content[j] === data[i].content[j].toUpperCase()) {
+                const randChar = characters
+                  .charAt(Math.floor(Math.random() * characters.length))
+                  .toUpperCase();
+                editedContent += randChar;
+              } else {
+                const randChar = characters
+                  .charAt(Math.floor(Math.random() * characters.length))
+                  .toLowerCase();
+                editedContent += randChar;
+              }
+            } else {
+              editedContent += data[i].content[j];
+            }
+          }
+          setObfuscatedBookData((prevState) => {
+            return [
+              ...prevState,
+              { type: data[i].type, content: editedContent },
+            ];
+          });
+        } else {
+          setObfuscatedBookData((prevState) => {
+            return [
+              ...prevState,
+              { type: data[i].type, content: data[i].content },
+            ];
+          });
+        }
+      }
+    }
+  }
+
+  // GET request function to backend to fetch data
   function getData() {
+    setIsLoading(true);
     axios
       .get("http://localhost:5000/notion-data", {
         params: {
           id: bookDetails.notionPageId,
-          // id: "c47e8eba8c5e4e18ae760376a7a4a67e",
         },
       })
-      .then((res) => setBookData(res.data));
+      .then((res) => {
+        setBookData(res.data);
+        obfuscateData(res.data);
+      })
+      .then(setIsLoading(false));
   }
 
-  // Get data on mount
+  // Call GET request on mount
   useEffect(() => {
     getData();
   }, []);
 
-  console.log(bookData);
-
   return (
     <div className="text-zinc-700">
-      {bookData.map((item, index) => {
-        if (item.type === "heading_1") {
-          return <HeadingOne key={index} content={item.content} />;
-        } else if (item.type === "heading_2") {
-          return <HeadingTwo key={index} content={item.content} />;
-        } else if (item.type === "heading_3") {
-          return <HeadingThree key={index} content={item.content} />;
-        } else if (item.type === "paragraph") {
-          return <Paragraph key={index} content={item.content} />;
-        }
-      })}
+      {isLoading ? (
+        <p className="h-[240px] bg-zinc-200 font-bold text-5xl text-center pt-[96px] my-24">
+          Loading...
+        </p>
+      ) : (
+        obfuscatedBookData.map((item, index) => {
+          if (item.type === "heading_1") {
+            return <HeadingOne key={index} content={item.content} />;
+          } else if (item.type === "heading_2") {
+            return <HeadingTwo key={index} content={item.content} />;
+          } else if (item.type === "heading_3") {
+            return <HeadingThree key={index} content={item.content} />;
+          } else if (item.type === "paragraph") {
+            return <Paragraph key={index} content={item.content} />;
+          }
+        })
+      )}
     </div>
   );
 }
